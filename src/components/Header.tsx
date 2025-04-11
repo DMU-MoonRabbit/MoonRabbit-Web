52a7c7ca23141ce0e1a814c84702db57e89b53e9
HYEON <titeotty12@gmail.com>
2025-04-12 02:43:08 +0900

    build: tailwind v4버전으로 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,29 @@
-import React, { JSX, useState, useEffect, useRef } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import MoonStar from '../assets/MoonStar.png'
 
-export const Header = (): JSX.Element => {
-    return (
-        <>
-        <div className='relative'>
-            <div className='fixed top-0 left-0 right-0'>
+const Header = () => {
+  return (
+    <div className="bg-darkWalnut text-darkBeige h-14 flex items-center justify-between px-8 shadow-md">
+      <div className="flex items-center space-x-10">
+        <Link to="/">
+          <img src={MoonStar} alt="MoonStar" />
+        </Link>
 
-            </div>
-        </div>
-        </>
-    )
+        <nav className="flex font-mainFont text-sm tracking-wide">
+          <Link to="/sky">밤하늘</Link>
+          <Link to="/question">오늘의 질문</Link>
+          <Link to="/stars">별자리</Link>
+          <Link to="/shop">상점</Link>
+        </nav>
+      </div>
+
+      <div className="flex items-center space-x-6 text-sm font-mainFont">
+        <Link to="/login">로그인 / 회원가입</Link>
+        <Link to="/settings">설정</Link>
+      </div>
+    </div>
+  )
 }
+
+export default Header
---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: '#E25F47',
+        lightBeige: '#FDF7EA',
+        darkBeige: '#F0D093',
+        lightCaramel: '#D48C5B',
+        lightWalnut: '#937066',
+        darkWalnut: '#473C2C',
+        mainWhite: '#F9F9FA',
+        subWhite: '#F5F5F5',
+        mainBlack: '#17171B',
+        subBlack: '#26262D',
+        mainGray: '#A8A8A8',
+      },
+      fontFamily: {
+        mainFont: ['HSBombaram2.1'],
+      },
+    },
+  },
+  plugins: [],
+}
