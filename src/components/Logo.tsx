0b266cc74548e48b5ec89384cbe600ae744439e4
HYEON <titeotty12@gmail.com>
2025-05-12 14:55:42 +0900

    refactor: 로고 컴포넌트 리팩토링

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,53 +1,52 @@
 import React from 'react';
+import clsx from 'clsx';
 import { useResponsiveStore } from '../stores/useResponsiveStore';
 
 const Logo: React.FC = () => {
   const res = useResponsiveStore((state) => state.res);
-  
-  // 화면 크기에 따라 로고 크기 조정
-  const logoWidth = res === 'pc' ? '569px' : '80%';
-  const logoHeight = res === 'pc' ? '825px' : 'auto';
-  
-  // 텍스트 크기 조정
-  const titleSize = res === 'pc' ? 'text-5xl' : 'text-3xl';
-  const subtitleSize = res === 'pc' ? 'text-lg' : 'text-base';
 
   return (
-    <div 
-      className="flex flex-col items-center justify-center" 
-      style={{ 
-        width: logoWidth, 
-        height: logoHeight,
+    <div
+      className="flex flex-col items-center justify-center"
+      style={{
+        width: res === 'pc' ? '569px' : '80%',
+        height: res === 'pc' ? '825px' : 'auto',
         maxWidth: '569px',
-        margin: '0 auto'
+        margin: '0 auto',
       }}
     >
       <img
-        src="/images/MoonRabbitLogo.png"
+        src="/images/MoonRabbitSleep2.png"
         alt="달토끼 로고"
         className="w-full h-auto object-contain mt-12"
-        style={{ 
+        style={{
           maxWidth: '569px',
-          maxHeight: '642px'
+          maxHeight: '642px',
         }}
       />
       <div className="text-center mt-16 flex flex-col items-center justify-center w-full">
-        <h1 
-          className={`${titleSize} font-mainFont text-center`} 
-          style={{ 
+        <h1
+          className={clsx(
+            'font-mainFont text-center',
+            res === 'pc' ? 'text-5xl' : 'text-3xl'
+          )}
+          style={{
             textAlign: 'center',
             margin: '0 auto',
-            color: 'var(--color-darkWalnut)'
+            color: 'var(--color-darkWalnut)',
           }}
         >
           <span style={{ color: 'var(--color-lightCaramel)' }}>달</span>토끼
         </h1>
-        <p 
-          className={`${subtitleSize} mt-2 font-mainFont text-center`} 
-          style={{ 
+        <p
+          className={clsx(
+            'mt-2 font-mainFont text-center',
+            res === 'pc' ? 'text-lg' : 'text-base'
+          )}
+          style={{
             textAlign: 'center',
             margin: '0 auto',
-            color: 'var(--color-darkWalnut)'
+            color: 'var(--color-darkWalnut)',
           }}
         >
           <span style={{ color: 'var(--color-lightCaramel)' }}>Moon</span>Rabbit
@@ -57,4 +56,4 @@ const Logo: React.FC = () => {
   );
 };
 
-export default Logo; 
+export default Logo;
