7518a3b35e94473ec4d6c9af97948e8f97dc0592
sooh329 <glay@kakao.com>
2025-06-10 19:14:01 +0900

    chore: 헤더 마이페이지에서 닉네임으로 변경

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -2,28 +2,32 @@ import { create } from 'zustand'
 
 interface UserState {
   email: string
+  nickname: string
   phoneNum: string
   verification: string
   password: string
   passwordConfirm: string
   setEmail: (email: string) => void
+  setNickname: (nickname: string) => void
   setPhoneNum: (phoneNum: string) => void
   setVerification: (verification: string) => void
   setPassword: (password: string) => void
   setPasswordConfirm: (passwordConfirm: string) => void
 }
 
-const useAuthStore = create<UserState>((set) => ({
+const useUserStore = create<UserState>((set) => ({
   email: '',
+  nickname: '',
   phoneNum: '',
   verification: '',
   password: '',
   passwordConfirm: '',
   setEmail: (email) => set({ email }),
+  setNickname: (nickname) => set({ nickname }),
   setPhoneNum: (phoneNum) => set({ phoneNum }),
   setVerification: (verification) => set({ verification }),
   setPassword: (password) => set({ password }),
   setPasswordConfirm: (passwordConfirm) => set({ passwordConfirm }),
 }))
 
-export default useAuthStore
+export default useUserStore
